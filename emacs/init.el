0a3da8f9a31f6cb8e80b66bd137a93267e5e2a5c
Mark Par <mrbpar@gmail.com>
2022-07-29 06:14:15 +0000

    initial emacs init file

---
+++ emacs/init.el
@@ -0,0 +1,78 @@
+;;; init.el --- Emacs initialization file
+
+;;; Commentary:
+;; 
+
+;;; Code:
+;;
+(provide 'init)
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t
+      use-package-expand-minimally t)
+
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
+(use-package lsp-mode
+  :defer t
+  :commands (lsp lsp-deferred)
+  :init (setq lsp-keymap-prefix "C-c l")
+  :hook
+  (python-mode . lsp-deferred)
+  (terraform-mode . lsp))
+
+(use-package lsp-ui
+  :defer t
+  :config
+  (setq lsp-ui-sideline-enable nil)
+  (setq lsp-ui-doc-delay 2)
+  :hook (lsp-mode . lsp-ui-mode)
+  :bind (:map lsp-ui-mode-map
+              ("C-c i" . lsp-ui-imenu)))
+
+(use-package treemacs
+  :defer t
+  :config
+  (setq treemacs-no-png-images t)
+  (setq treemacs-width 25)
+  :bind ("C-c t" . treemacs))
+
+(use-package flycheck
+  :init
+  (global-flycheck-mode))
+
+(use-package terraform-mode)
+(use-package yaml-mode)
+
+(use-package magit)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(auto-package-update use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;; init.el ends here
