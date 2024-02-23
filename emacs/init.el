bb8886febc7658a02ca9c1fc20e1bb1fcb810d59
Mark Par <mrbpar@gmail.com>
2024-02-23 09:05:18 +0000

    add gruvbox theme checksum

--- emacs/init.el
+++ emacs/init.el
@@ -100,6 +100,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" default))
  '(package-selected-packages
    '(gruvbox-theme helm solarized-theme auto-package-update use-package)))
 (custom-set-faces
